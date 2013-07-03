3ec5c9cbc2cdcabf5b2887e09ffcefc595ce25ac
Leandro Souza <leandro.souza@corp.globo.com>
2013-07-03 14:57:56 -0300

    new version with filter content_type

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ STATUS_PROD = 'Development Status :: 5 - Production/Stable'
 STATUS_BETA = 'Development Status :: 4 - Beta'
 STATUS_ALPHA = 'Development Status :: 3 - Alpha'
 
-version = '0.0.4'
+version = '0.0.5'
 README = os.path.join(os.path.dirname(__file__), 'README.rst')
 long_description = open(README).read()
 setup(
