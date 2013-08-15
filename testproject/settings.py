ad2332e1c5f76ef5e13e07dcbb596f6dffd07628
Leandro Souza <leandro.souza@corp.globo.com>
2013-08-15 16:39:34 -0300

    middleware name

--- testproject/settings.py
+++ testproject/settings.py
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'simple_audit.middleware.threadlocals.RequestThreadLocalMiddleware',
+    'simple_audit.middleware.TrackingRequestOnThreadLocalMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
