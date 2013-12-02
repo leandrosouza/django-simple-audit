f137520233f6fa5b2f5b82e05419c7206b7c2771
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-12-02 14:32:43 -0200

    rename test_app to simple_app

--- testproject/settings.py
+++ testproject/settings.py
@@ -128,7 +128,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'test_app',
+    'simple_app',
     'simple_audit',
     'django_extensions',
 )
