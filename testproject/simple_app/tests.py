d70a746b56a14b3682e37b15f5cc7efef54bffc3
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-12-02 15:06:06 -0200

    validating audits in tests

--- testproject/simple_app/tests.py
+++ testproject/simple_app/tests.py
@@ -6,7 +6,9 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.contrib.contenttypes.models import ContentType
 from .models import Topping, Pizza
+from simple_audit.models import Audit
 
 class SimpleTest(TestCase):
 
@@ -14,27 +16,59 @@ class SimpleTest(TestCase):
         self.topping_onion = Topping.objects.get_or_create(name="onion")[0]
         self.topping_egg = Topping.objects.get_or_create(name="egg")[0]
 
-    def test_add_topping(self):
+        self.content_type_topping = ContentType.objects.get_for_model(Topping)
+        self.content_type_pizza = ContentType.objects.get_for_model(Pizza)
+
+    def test_add_topping_and_search_audit(self):
         """tests add a topping"""
-        topping = Topping.objects.get_or_create(name="onion")[0]
+        topping = Topping.objects.get_or_create(name="potato")[0]
+        
+        #topping created
         self.assertTrue(topping.pk)
+        #audit recorded?
+        self.assertTrue(Audit.objects.get(operation=0, 
+                                            content_type=self.content_type_topping,
+                                            object_id=topping.pk,
+                                            description="Added potato"))
 
     def test_add_pizza_without_toppings(self):
         """test add pizza without topping"""
         pizza = Pizza.objects.get_or_create(name="mussarela")[0]
-
+        
+        #pizza created?
         self.assertTrue(pizza.pk)
-
+        #toppings added?
         self.assertEqual(pizza.toppings.all().count(), 0)
+        #audit recorded?
+        self.assertTrue(Audit.objects.get(operation=0, 
+                                            content_type=self.content_type_pizza,
+                                            object_id=pizza.pk,
+                                            description="Added mussarela"))
 
 
-    def test_add_pizza_with_toppings(self):
+    def test_add_pizza_with_toppings_with_audit_enabled(self):
         """test add pizza with topping"""
-        pizza = Pizza.objects.get_or_create(name="calabresa")[0]
 
+        pizza = Pizza.objects.get_or_create(name="peperoni")[0]
+        
+        #pizza created?
         self.assertTrue(pizza.pk)
-
+        #toppings added?
         pizza.toppings.add(self.topping_onion)
-        pizza.toppings.add(self.topping_egg)
 
-        self.assertEqual(pizza.toppings.all().count(), 2)
+        self.assertEqual(pizza.toppings.all().count(), 1)
+        
+        #audit recorded?
+        self.assertTrue(Audit.objects.get(operation=0, 
+                                            content_type=self.content_type_pizza,
+                                            object_id=pizza.pk,
+                                            description="Added peperoni"))
+        
+        #m2m audit recorded?
+        # field toppings: was changed from None to [{u'id': 2, 'name': u'ovo'}]
+        self.assertTrue(Audit.objects.get(operation=1, 
+                            content_type=self.content_type_pizza,
+                            object_id=pizza.pk,
+                            description="field toppings: was changed from None to [{u'id': %s, 'name': u'%s'}]" % (self.topping_onion.id, self.topping_onion.name)))
+
+
