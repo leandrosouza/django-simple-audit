1a67704b9bbb6c62c49daaef4f3299ce6124ad04
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-12-03 17:42:03 -0200

    fix make_tag script

--- make_tag.sh
+++ make_tag.sh
@@ -2,12 +2,14 @@
 
 VERSION=$(grep "version = " setup.py  | cut -d "=" -f2 | cut -d "'" -f2)
 
-echo $VERSION
+echo "VERSION: ${VERSION}"
 
-if ! git tag | grep $VERSION | grep -v grep;
+if ! git tag | grep $VERSION | grep -v grep > /dev/null;
 then
     echo "TAG ${VERSION} does not exists!"
     git tag ${VERSION}
     git push --tags
     git push
+else
+    echo "TAG ${VERSION} already exists!"
 fi
