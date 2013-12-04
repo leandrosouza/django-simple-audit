d47eb859998833d78c4a3b1707a9973afd412e39
Gustavo Soares Souza <gustavosoares@gmail.com>
2013-12-04 14:36:58 -0200

    checking if cache backend

--- simple_audit/settings.py
+++ simple_audit/settings.py
@@ -1,4 +1,11 @@
 """Settings for django-simple-audit"""
+import logging
 from django.conf import settings
 
+LOG = logging.getLogger(__name__)
+
 DJANGO_SIMPLE_AUDIT_M2M_FIELDS = getattr(settings, 'DJANGO_SIMPLE_AUDIT_M2M_FIELDS', False)
+
+if not hasattr(settings, 'CACHES'):
+    LOG.warning("no cache backend set in django! m2m auditing will be disabled")
+    DJANGO_SIMPLE_AUDIT_M2M_FIELDS = False
