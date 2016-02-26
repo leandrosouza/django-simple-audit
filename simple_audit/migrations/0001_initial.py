7aa18f08c116f8e1045666c3a6954dd0d2761a04
Scott Burns <scott.s.burns@gmail.com>
2016-02-26 15:48:49 -0600

    Add migrations

---
+++ simple_audit/migrations/0001_initial.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Audit',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date', models.DateTimeField(auto_now_add=True, verbose_name='Date')),
+                ('operation', models.PositiveIntegerField(max_length=255, verbose_name='Operation', choices=[(0, 'add'), (1, 'change'), (2, 'delete')])),
+                ('object_id', models.PositiveIntegerField()),
+                ('description', models.TextField()),
+                ('obj_description', models.CharField(db_index=True, max_length=100, null=True, blank=True)),
+            ],
+            options={
+                'db_table': 'audit',
+                'verbose_name': 'Audit',
+                'verbose_name_plural': 'Audits',
+            },
+        ),
+        migrations.CreateModel(
+            name='AuditChange',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('field', models.CharField(max_length=255)),
+                ('old_value', models.TextField(null=True, blank=True)),
+                ('new_value', models.TextField(null=True, blank=True)),
+                ('audit', models.ForeignKey(related_name='field_changes', to='simple_audit.Audit')),
+            ],
+            options={
+                'db_table': 'audit_change',
+                'verbose_name': 'Audit',
+                'verbose_name_plural': 'Audits',
+            },
+        ),
+        migrations.CreateModel(
+            name='AuditRequest',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('request_id', models.CharField(max_length=255)),
+                ('ip', models.IPAddressField()),
+                ('path', models.CharField(max_length=1024)),
+                ('date', models.DateTimeField(auto_now_add=True, verbose_name='Date')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'db_table': 'audit_request',
+                'verbose_name': 'Audit',
+                'verbose_name_plural': 'Audits',
+            },
+        ),
+        migrations.AddField(
+            model_name='audit',
+            name='audit_request',
+            field=models.ForeignKey(to='simple_audit.AuditRequest', null=True),
+        ),
+        migrations.AddField(
+            model_name='audit',
+            name='content_type',
+            field=models.ForeignKey(to='contenttypes.ContentType'),
+        ),
+    ]
